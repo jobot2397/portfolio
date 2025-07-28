3f0f3b2c2cb01f8421738f1a992c5ebebb00b25b
Joseph Lawler <gitlab@jfl.services>
2025-07-27 20:40:42 -0500

    Basic sidebar and sizing for mobile and desktop

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,13 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
 });
